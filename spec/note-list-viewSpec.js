30be06321989d647f86cbfeea6e71c6cdd0b6364
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-03 15:23:54 +0000

    Add method to display a list of notes

--- spec/note-list-viewSpec.js
+++ spec/note-list-viewSpec.js
@@ -1,9 +1,36 @@
 'use strict';
 
-function acceptsNoteListModel(){
+function testAcceptsNoteListModel(){
   var noteList = new NoteList();
   var noteListView = new NoteListView(noteList);
   assert.isTrue(noteListView.model === noteList);
 }
 
-acceptsNoteListModel();
+testAcceptsNoteListModel();
+
+function testDisplayNoteList(){
+  var noteList = new NoteList();
+  noteList.createNote("Test List 1");
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.displayNoteList() === "<ul><li><div>Test List 1</div></li></ul>")
+}
+
+testDisplayNoteList();
+
+function testDisplayTwoNoteList(){
+  var noteList = new NoteList();
+  noteList.createNote("Test List 1");
+  noteList.createNote("Test List 2");
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.displayNoteList() === "<ul><li><div>Test List 1</div></li><li><div>Test List 2</div></li></ul>")
+}
+
+testDisplayTwoNoteList();
+
+function testDisplayZeroNoteList(){
+  var noteList = new NoteList();
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.displayNoteList() === "<ul></ul>")
+}
+
+testDisplayZeroNoteList();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -6,6 +6,15 @@
     this.model = noteListModel;
   }
 
+  NoteListView.prototype.displayNoteList = function(){
+    var output = "<ul>";
+    this.model.list.forEach(function(note){
+      output += "<li><div>" + note.text + "</div></li>";
+    })
+    output += "</ul>";
+    return output;
+  }
+
   exports.NoteListView = NoteListView;
 
 })(this);
