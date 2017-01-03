392adbe3d44728d3e7feabd8bd5116adf67ca281
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-03 15:49:28 +0000

    Note app has a method for returning text property

--- note-model-spec.js
+++ note-model-spec.js
@@ -2,5 +2,10 @@ function testNoteCreation() {
   var note = new Note("test");
   assert.isTrue(note._text === "test");
 };
-
 testNoteCreation();
+
+function testReturnNoteText() {
+  var note = new Note("test");
+  assert.isTrue(note.returnNoteText() === note._text)
+};
+testReturnNoteText();
--- note-model.js
+++ note-model.js
@@ -1,3 +1,7 @@
 function Note(string){
   this._text = string;
 }
+
+Note.prototype.returnNoteText = function() {
+  return this._text;
+};
