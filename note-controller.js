a37baffe98f238a4da2474d27a2ba5c50e6f9315
Kim Wilson <k.wilson541@gmail.com>
2017-01-03 17:24:34 +0000

    Added method to note controller that displays list of notes

--- note-controller.js
+++ note-controller.js
@@ -1,4 +1,18 @@
-function getDivHTML() {
-  var appHTML = document.getElementById("app")
-  appHTML.innerHTML = "Howdy"
-}
+'use strict';
+
+(function(exports){
+
+	function NoteController(noteListModel){
+		this.model = noteListModel;
+		this.model.createNote("Favourite Drink: Margarita");
+		this.noteList = new NoteListView(this.model);
+	}
+
+	NoteController.prototype.changeHTML = function() {
+		var appHTML = document.getElementById("app");
+		appHTML.innerHTML = this.noteList.displayNoteList();
+	}
+
+	exports.NoteController = NoteController;
+
+})(this);
