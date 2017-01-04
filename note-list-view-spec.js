24965887a70205127ec02a6c136f8c348364632f
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-04 11:03:00 +0000

    Tested for viewing notelists with multiple and with no notes

--- note-list-view-spec.js
+++ note-list-view-spec.js
@@ -3,10 +3,24 @@ note.returnNoteText = function(){
   return "A doubled note";
 };
 
-noteList = {};
+note2 = {};
+note2.returnNoteText = function(){
+  return "A secondouble note";
+};
 
+noteList = {};
 noteList.showNotes = function(){
   return [note];
+};
+
+noteList2 = {};
+noteList2.showNotes = function(){
+  return [note, note2];
+};
+
+noteList3 = {};
+noteList3.showNotes = function(){
+  return [];
 };
 
 function testInstantiatesWithNoteList(){
@@ -20,3 +34,15 @@ function testListNotes(){
   assert.isTrue(noteListView.listNotes() === "<ul><li><div>A doubled note</div></li></ul>");
 };
 testListNotes();
+
+function testMultipleListNotes(){
+  var noteListView = new NoteListView(noteList2);
+  assert.isTrue(noteListView.listNotes() === "<ul><li><div>A doubled note</div></li><li><div>A secondouble note</div></li></ul>");
+};
+testMultipleListNotes();
+
+function testEmptyListNotes(){
+  var noteListView = new NoteListView(noteList3);
+  assert.isTrue(noteListView.listNotes() === "<ul></ul>");
+};
+testMultipleListNotes();
