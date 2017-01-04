ff7cad6741f243ff06faf4ca832b1f62b263d8da
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-04 15:42:31 +0000

    TTDd NoteController DOM

---
+++ note-controller-spec.js
@@ -0,0 +1,27 @@
+note = {};
+note.returnNoteText = function(){
+  return "Favourite drink: margarita";
+};
+
+noteList = {};
+noteList.showNotes = function(){
+  return [note];
+};
+
+function testInstantiateNoteController() {
+  var controller = new NoteController(noteList);
+  assert.isTrue(typeof controller === "object")
+};
+testInstantiateNoteController();
+
+function testUpdateHTML() {
+  var controller = new NoteController(noteList);
+
+  controller.addNoteListView();
+  var app = document.createElement("div");
+  app.id = "app";
+  document.body.appendChild(app);
+  controller.updateHTML("app");
+  assert.isTrue(app.innerHTML === "<ul><li><div>Favourite drink: margarita</div></li></ul>")
+}
+testUpdateHTML();
--- note-controller.js
+++ note-controller.js
@@ -1,21 +1,22 @@
 function NoteController(noteList){
-	this._noteList = noteList;
+  this._noteList = noteList;
 }
-
 (function(exports) {
+  NoteController.prototype.addNote = function(text){
+    this._noteList.createNote(text)
+  };
 
-	NoteController.prototype.addNote = function(text){
-		this._noteList.createNote(text);
-	};
+  NoteController.prototype.addNoteListView = function(){
+    this._noteListView = new NoteListView(this._noteList);
+  };
 
-	NoteController.prototype.addNoteListView = function(){
-		this.noteListView = new NoteListView(this._noteList);
-	};
+  NoteController.prototype.updateHTML = function(id){
+    var getListNotes = this._noteListView.listNotes();
+    var notesDisplay = document.getElementById(id);
+    notesDisplay.innerHTML = getListNotes;
+  };
 
-	NoteController.prototype.updateHTML = function(){
-		var getListNotes = this.noteListView.listNotes();
-		var noteDisplay = document.getElementById("app");
-		noteDisplay.innerHTML = getListNotes;
-	};
-
+  exports.addNote = this.addNote;
+  exports.addNoteListView = this.addNoteListView;
+  exports.updateHTML = this.updateHTML;
 })(this);
