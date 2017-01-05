ef8517e3622c707cc810408a2761b773eb538e79
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-05 16:13:42 +0000

    BOOYAH SINGLE PAGE - click note link shows text, wo page refresh

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ noteController = new NoteController(noteList);
 noteController.addNote("Favourite drink: carrot juice");
 noteController.addNoteListView();
 noteController.updateHTML("app");
+
+noteController.makeURLChangeShowNoteInCurrentPage();
--- src/controllers/note-controller.js
+++ src/controllers/note-controller.js
@@ -18,6 +18,30 @@
     noteDisplay.innerHTML = getListNotes;
   };
 
+  NoteController.prototype.makeURLChangeShowNoteInCurrentPage = function(){
+    window.addEventListener("hashchange", this.showNoteForCurrentPage.bind (this));
+  };
+
+  NoteController.prototype.showNoteForCurrentPage = function(){
+    this.findNote(this.getNoteFromURL(window.location));
+  };
+
+  NoteController.prototype.getNoteFromURL = function(location){
+    return location.hash.split("#")[1];
+  };
+
+  NoteController.prototype.findNote = function(id){
+    note = this._noteList._notes[id];
+    singleNoteView = new SingleNoteView(note)
+    this.showNote(singleNoteView);
+  };
+
+  NoteController.prototype.showNote = function(note) {
+    document
+    .getElementById("app")
+    .innerHTML = note.viewNote();
+  };
+
   exports.NoteController = NoteController;
 
 })(this);
